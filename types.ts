3970edfad1499bed3c753fbd2d624873c69bfeb6
Pham Viet Hoa <phamviethoa2416@gmail.com>
2025-01-03 07:30:04 +0700

    feat: add interfaces

---
+++ types.ts
@@ -0,0 +1,62 @@
+import Stripe from "stripe";
+
+export interface UserDetails {
+    id: string;
+    first_name: string;
+    last_name: string;
+    full_name?: string;
+    avatar_url?: string;
+    billing_address?: Stripe.Address;
+    payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type]
+}
+
+export interface Product {
+    id: string;
+    active?: boolean;
+    name?: string;
+    description?: string;
+    images?: string;
+    metadata?: string;
+}
+
+export interface Price {
+    id: string;
+    product_id?: string;
+    active?: string;
+    description?: string;
+    unit_amount?: number;
+    currency?: string;
+    type?: Stripe.Price.Type;
+    interval?: Stripe.Price.Recurring.Interval;
+    interval_count?: number;
+    trial_period_days?: number | null;
+    metadata?: Stripe.Metadata;
+    product?: Product;
+}
+
+export interface Subscription {
+    id: string;
+    user_id: string;
+    status?: Stripe.Subscription.Status;
+    price_id?: string;
+    quantity?: number;
+    cancel_at_period_end?: boolean;
+    created: string;
+    current_period_start: string;
+    current_period_end: string;
+    ended_at?: string;
+    cancel_at?: string;
+    canceled_at?: string;
+    trial_start?: string;
+    trial_end?: string;
+    price?: Price;
+}
+
+export interface Song {
+    id: string;
+    user_id: string;
+    artist: string;
+    title: string;
+    song_path: string;
+    image_path: string;
+}
